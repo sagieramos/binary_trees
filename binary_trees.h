512812b48431348420ac4e45311cbbd90276d56a
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-12-15 12:23:33 +0100

    binary_tree

--- binary_trees.h
+++ binary_trees.h
@@ -17,12 +17,11 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
 #endif /* _BINARY_TREES_H_ */
